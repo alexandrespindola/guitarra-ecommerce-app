60142055405b80e93eb92a6c2102461de8a7e93d
Alexandre Spindola <alexandre.spindola11@gmail.com>
2024-03-18 22:17:24 +0100

    Adiciona dependências de Sass, Autoprefixer, PostCSS e TailwindCSS e estilizações em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className="flex flex-row justify-center">
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello React</h1>
+      <h1 className="bg-blue-800 rounded-2xl p-4">Hello React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
